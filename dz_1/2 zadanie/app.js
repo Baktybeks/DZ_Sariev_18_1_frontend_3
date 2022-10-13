d9de26c7c53d70813065be9d2aede3c49b6885e2
Baktybeks <baktybeks@mail.ru>
2022-10-13 18:33:21 +0600

    dz2

--- dz_1/2 zadanie/app.js
+++ dz_1/2 zadanie/app.js
@@ -1,26 +1,26 @@
 let smallBox = document.querySelector(".smallBox");
 let moveLeft = 0;
 
-const MoveLeft = () => {
-    moveLeft += 20
-    smallBox.addEventListener("click", () => {
-        if (moveLeft < 500-50-20) {
-            smallBox.style.left = `${moveLeft}px`
-            console.log("if=", moveLeft)
-            MoveLeft()
-        }
-    })
-};
-MoveLeft()
-
-
 // const MoveLeft = () => {
-//     if (moveLeft < 500-50-20) {
-//         moveLeft += 20
-//         smallBox.addEventListener("click", () => {
-//             smallBox.style.left = ${moveLeft}px
-//         })
-//         MoveLeft();
-//     }
-// }
+//     moveLeft += 20
+//     smallBox.addEventListener("click", () => {
+//         if (moveLeft < 500-50-20) {
+//             smallBox.style.left = `${moveLeft}px`
+//             console.log("if=", moveLeft)
+//             MoveLeft()
+//         }
+//     })
+// };
 // MoveLeft()
+
+
+const MoveLeft = () => {
+    if (moveLeft < 500-50-20) {
+        moveLeft += 20
+        smallBox.addEventListener("click", () => {
+            smallBox.style.left = `${moveLeft}px`
+        })
+        MoveLeft();
+    }
+}
+MoveLeft()
